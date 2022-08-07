71bc0c7acfe32bb57540d165d50d965ecbc60118
HAN XU <hk772400287@yahoo.co.jp>
2022-08-08 01:41:23 +0900

    hw2

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -53,15 +53,14 @@ public class PercolationStats {
         return μ + 1.96 * σ /  java.lang.Math.sqrt(T);
     }
 
-    public static void main(String[] args) {
-        // use for unit testing (not required)
-        PercolationStats per = new PercolationStats(20, 10, new PercolationFactory());
-        for (double x : per.xt) {
-            System.out.println(x);
-        }
-        System.out.println(per.mean());
-        System.out.println(per.stddev());
-
-    }
+//    public static void main(String[] args) {
+//        // use for unit testing (not required)
+//        PercolationStats per = new PercolationStats(20, 10, new PercolationFactory());
+//        for (double x : per.xt) {
+//            System.out.println(x);
+//        }
+//        System.out.println(per.mean());
+//        System.out.println(per.stddev());
+//    }
 
 }
