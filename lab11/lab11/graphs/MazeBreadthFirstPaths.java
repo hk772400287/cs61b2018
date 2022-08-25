07be0ab71193b55a3bdfbd3cb28c6d31784b973c
HAN XU <hk772400287@yahoo.co.jp>
2022-08-26 00:29:41 +0900

    lab11

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,10 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.In;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +14,52 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private Maze maze;
+    private Deque<Integer> queue;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
+        this.queue = new ArrayDeque<>();
+
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        // Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        this.marked[s] = true;
+        announce();
+        if (s == t) {
+            return;
+        }
+        this.queue.add(this.s);
+        while (!this.queue.isEmpty()) {
+            int x = queue.poll();
+            for (int w : this.maze.adj(x)) {
+                if (!this.marked[w]) {
+                    this.queue.add(w);
+                    this.marked[w] = true;
+                    this.distTo[w] = this.distTo[x] + 1;
+                    this.edgeTo[w] = x;
+                    announce();
+                    if (w == t) {
+                        return;
+                    }
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
